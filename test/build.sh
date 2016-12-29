ab8a4aa80509db7ffa076a7f0a743502e07e27ee
lcit <ldo.citraro@gmail.com>
2016-12-29 05:41:33 -0800

    refactoring + readme

--- test/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rdf ./build > /dev/null 2>&1
-rm output* > /dev/null 2>&1
-mkdir build 
-cd build
-cmake ..
-make
-cd ..
--- test/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rdf ./build > /dev/null 2>&1
-rm -rdf ./*~ > /dev/null 2>&1
-rm output* > /dev/null 2>&1
